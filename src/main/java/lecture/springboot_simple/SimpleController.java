2a96cf4a8fb67e4369fc1d4f7f43341ee1264a22
kriyaTechno <108522556+kriyaTechno@users.noreply.github.com>
2024-06-05 11:11:05 +0530

    first commit

---
+++ src/main/java/lecture/springboot_simple/SimpleController.java
@@ -0,0 +1,42 @@
+package lecture.springboot_simple;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+// trigger ci again and again
+
+@RestController
+public class SimpleController {
+
+    @GetMapping(value = { "/", "/datetime" })
+    public ResponseEntity<String> getDateTime() {
+        String res = "Server Time: " + new Date().toString();
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/greet/{name}")
+    public ResponseEntity<String> greetUser(@PathVariable String name) {
+        String res = "Hello " + name;
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/aston")
+    public ResponseEntity<String> randomAscii() {
+        return ResponseEntity.ok(generateRandomAscii(10000));
+    }
+
+    public String generateRandomAscii(int numOfChars) {
+//        String availChars = "_-/\\|(),.<>;:'=+!@#$%^&*";
+        String availChars = "_-/\\|()";
+        StringBuilder bigString = new StringBuilder();
+
+        for (int i = 0; i < numOfChars; i++) {
+            int randomIndex = (int) (Math.random() * availChars.length());
+            bigString.append(availChars.charAt(randomIndex));
+        }
+
+        return bigString.toString();
+    }
+}
